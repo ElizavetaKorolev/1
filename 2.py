6f8c5682c1e13152936b9e711d0f74c360d62765
Elizaveta Koroleva <lizakoroleva026@mail.ru>
2025-12-03 11:15:57 +0300

    *+1*

--- 2.py
+++ 2.py
@@ -1,14 +1,16 @@
-n = int(input("Введите количество чисел Фибоначчи: "))
-if n <= 0:
-    fib = []
-elif n == 1:
-    fib = [1]
-elif n == 2:
-    fib = [1, 1]
-else:
-    fib = [1, 1]
-    for i in range(2, n):
-        fib.append(fib[i-1] + fib[i-2])
+n_str = input("Введите количество чисел Фибоначчи: ")
+if n_str.isdigit() and int(n_str) > 0:
+    n = int(n_str)
+    if n == 1:
+        fib = [1]
+    elif n == 2:
+        fib = [1, 1]
+    else:
+        fib = [1, 1]
+        for i in range(2, n):
+            fib.append(fib[i - 1] + fib[i - 2])
 
-print("Первые", n, "чисел Фибоначчи:")
-print(fib[:n])
+    print("Первые", n, "чисел Фибоначчи:")
+    print(fib)
+else:
+    print("Ошибка: нужно ввести целое положительное число.")
