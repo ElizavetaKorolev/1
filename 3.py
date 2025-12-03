f76a4d2dacaa33d5ee49a83575c1c34435eaa2e0
Elizaveta Koroleva <lizakoroleva026@mail.ru>
2025-12-03 11:18:23 +0300

    *+1*

--- 3.py
+++ 3.py
@@ -1,13 +1,17 @@
 text = input("Введите строку: ")
-k = int(input("Введите сдвиг: "))
-
+while True:
+    k_input = input("Введите сдвиг (целое число): ")
+    if k_input.lstrip('-').isdigit():
+        k = int(k_input)
+        break
+    else:
+        print("Ошибка! Пожалуйста, введите целое число.")
 result = ""
 for char in text:
-    if char.isalpha():
+    if char.isalpha() and char.isascii():
         start = ord('A') if char.isupper() else ord('a')
-        shifted = (ord(char) - start + k) % 26 + start
+        shifted = start + (ord(char) - start + k) % 26
         result += chr(shifted)
     else:
         result += char
-
 print("Зашифрованный текст:", result)
